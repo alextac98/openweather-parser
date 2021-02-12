516f3f117bed3aec4309259c081c1bfb48bd66b2
Alex Tacescu <alextac98@gmail.com>
2021-02-11 20:54:17 -0500

    Add debugging system for vscode

--- OpenWeather-Parser.py
+++ OpenWeather-Parser.py
@@ -1,4 +1,12 @@
-
+import json
 
 if __name__ == '__main__':
-    print("Hello World")
+    
+    with open('data/BU_Weather_1971-2021.json') as file:
+        raw_data = json.load(file)
+
+        for hour in raw_data:
+            print()
+
+    print()
+
